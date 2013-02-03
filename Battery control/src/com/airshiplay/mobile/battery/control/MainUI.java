0785da3208b74d99ed0d1e0144a063bfa440d36d
airshiplay <airshiplay@126.com>
2013-02-03 20:48:01 +0800

    update config

--- Battery control/src/com/airshiplay/mobile/battery/control/MainUI.java
+++ Battery control/src/com/airshiplay/mobile/battery/control/MainUI.java
@@ -1,24 +1,31 @@
 package com.airshiplay.mobile.battery.control;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
+/**
+ * @author li
+ *
+ */
 public class MainUI extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+	}
+
 }
