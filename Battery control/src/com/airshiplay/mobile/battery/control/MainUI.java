6235d3f189e530719740f583e39447acefa08d86
airshiplay <airshiplay@126.com>
2013-02-07 00:51:43 +0800

    update  code

--- Battery control/src/com/airshiplay/mobile/battery/control/MainUI.java
+++ Battery control/src/com/airshiplay/mobile/battery/control/MainUI.java
@@ -3,7 +3,10 @@ package com.airshiplay.mobile.battery.control;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager.LayoutParams;
 
 /**
  * @author lig
@@ -17,6 +20,15 @@ public class MainUI extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		startService(new Intent(this,BatteryService.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+		
+		Display display = getWindowManager().getDefaultDisplay(); // 为获取屏幕宽、高
+		Window window = getWindow();
+		LayoutParams windowLayoutParams = window.getAttributes(); // 获取对话框当前的参数值
+		windowLayoutParams.width =0;// (int) (display.getWidth() * 0.7); // 宽度设置为屏幕的0.95
+		windowLayoutParams.height = 0;//(int) (display.getHeight() * 0.1); // 高度设置为屏幕的0.6
+		windowLayoutParams.alpha = 0.5f;// 设置透明度
+		
+		finish();
 	}
 
 	@Override
