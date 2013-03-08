699226f27b611180b97566726d4653c9aadc18a0
airshiplay <airshiplay@126.com>
2013-03-08 19:22:40 +0800

    update add application

--- android-framework/src/com/airshiplay/mobile/android/framework/FrameWorkApplication.java
+++ android-framework/src/com/airshiplay/mobile/android/framework/FrameWorkApplication.java
@@ -1,14 +1,32 @@
 package com.airshiplay.mobile.android.framework;
 
+import android.app.Application;
+import android.view.WindowManager;
+
 import com.airshiplay.mobile.android.framework.util.Devices;
 
-import android.app.Application;
-
+/**
+ * @author airshiplay
+ * @Create Date 2013-3-8
+ * @version 1.0
+ * @since 1.0
+ */
 public class FrameWorkApplication extends Application {
+	private WindowManager.LayoutParams winLayoutParams = new WindowManager.LayoutParams();
+	private static FrameWorkApplication instance;
+
+	public static FrameWorkApplication getInstance() {
+		return instance;
+	}
+
+	public final WindowManager.LayoutParams getWinLayoutParams() {
+		return this.winLayoutParams;
+	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		instance = this;
 		Devices.init(this);
 	}
 }
