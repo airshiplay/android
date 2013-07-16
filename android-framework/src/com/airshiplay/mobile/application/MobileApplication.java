db770a388a69103b353a810ec32a6331c90a1d2e
airshiplay <airshiplay@126.com>
2013-07-16 09:07:40 +0800

    update

---
+++ android-framework/src/com/airshiplay/mobile/application/MobileApplication.java
@@ -0,0 +1,60 @@
+package com.airshiplay.mobile.application;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.Application;
+import android.view.WindowManager;
+
+import com.airshiplay.mobile.log.LoggerFactory;
+import com.airshiplay.mobile.util.ScreenUtil;
+
+/**
+ * @author airshiplay
+ * @Create Date 2013-3-8
+ * @version 1.0
+ * @since 1.0
+ */
+public class MobileApplication extends Application {
+	private WindowManager.LayoutParams winLayoutParams = new WindowManager.LayoutParams();
+	private static MobileApplication instance;
+	private List<Activity> listActivities;
+
+	public static MobileApplication getInstance() {
+		return instance;
+	}
+
+	public final WindowManager.LayoutParams getWinLayoutParams() {
+		return this.winLayoutParams;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		instance = this;
+		this.listActivities = new ArrayList<Activity>();
+		ScreenUtil.init(getApplicationContext());
+		LoggerFactory.init(getApplicationContext());
+	}
+
+	public void addAcitvity(Activity activity) {
+		this.listActivities.add(activity);
+	}
+
+	public void finishAllActivity() {
+		Iterator<Activity> itr = this.listActivities.iterator();
+		while (itr.hasNext())
+			(itr.next()).finish();
+		android.os.Process.killProcess(android.os.Process.myPid());
+	}
+
+	public List<Activity> getList() {
+		return this.listActivities;
+	}
+
+	public void remove(Activity activity) {
+		this.listActivities.remove(activity);
+	}
+}
